8d31af61d32d19c6dcd59d8fd4827d074dfbbf2b
sauerma <msauer1stud.hs-offenburg.de>
2021-04-30 19:38:37 +0200

    Navigationsleisten Items angelegt

---
+++ src/components/Planung.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Typography from '@material-ui/core/Typography';
+
+const Planung = () => {
+    return (
+        <div>
+            <Typography paragraph>
+            Planung
+            </Typography>
+            <Typography paragraph>
+            Planung
+            </Typography>
+        </div>
+    )
+}
+
+export default Planung
