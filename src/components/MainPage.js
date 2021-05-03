efb98379a40134091a554d8bdaa700054b9285c9
sauerma <msauer1stud.hs-offenburg.de>
2021-05-03 09:34:54 +0200

    Tabellen und Charts eingefügt

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,30 +1,12 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import './Style/MainPage.css';
+import Dashboard from './MainPage/Dashboard';
 
 const MainPage = () => {
     return (
-        <div className="maindiv">
-               
-      <Typography>
-
--- Startseite --
-
-</Typography>
-
-<Typography paragraph>     
-
-                  
-  - Auf der Startseite werden relevante Kennzahlen angezeigt. -- Auf Basis des Mockups implementieren. 
-  - Idee: Liniendigramm -- Y-Achse: Anzahl der produzierten Artikel. -- X-Achse: Zeit  
-  Weitere relevante Kennzahlen evenutell in KPI-Diagrammen: Produktionsauslastungsgrad, Produzierte Artikel letzter Tag/WocheMontag/Jahr,
-  Produktionsvolumen, Produktionsausfälle, Produktionsmängel 
-
-</Typography>
-  <img src="https://www.datapine.com/de/images/production-volume.png" alt="Beispielbild"/>
-<Typography paragraph>
-
-</Typography>
+        <div className="maindiv">          
+<Dashboard/>
 </div>
     )
 }
