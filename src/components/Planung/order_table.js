b3390dd19bb54b3e276c701eebeb495fb6a5ac5b
sauerma <msauer1stud.hs-offenburg.de>
2021-05-08 17:07:23 +0200

    add GetPlanningOrders

--- src/components/Planung/order_table.js
+++ src/components/Planung/order_table.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 
 export default function DataTable() {
   const columns = ["Bestelldatum", "Bestellnr", "Produktionsnr", "Menge", "Status", "Delta E", "Hex-Wert", "Farbe", "Priorität", "Bild"];
-  const data = [
+  var data = [
     ["2021-04-21 11:50:05", "B-20000000-1", "1", "200", "In Planung", "1.324234", "#34923", "Marine", "1", "/home/img/tshirt123123.png"],
     ["2021-04-21 11:50:05", "B-20000000-1", "2", "21", "In Planung", "1.324234", "#34923", "Rot", "1", "/home/img/tshirt123123.png"],
     ["2021-04-21 11:50:05", "B-20000000-1", "3", "2", "In Planung", "1.324234", "#34923", "Blau", "2", "/home/img/tshirt123123.png"],
@@ -15,25 +15,23 @@ export default function DataTable() {
   filterType: 'checkbox',
 };
 
-const restApi = axios.create({
-  baseURL: 'https://1ygz8xt0rc.execute-api.eu-central-1.amazonaws.com/main/getplanningorders'
-})
-  
 
 function getPlanningOrders()
-{ 
-  restApi.get('/').then(res => {
-    console.log(res.data)
-  }
+{
+  axios.get('https://1ygz8xt0rc.execute-api.eu-central-1.amazonaws.com/main/getplanningorders').then(
+    (res) => {
+      console.log(res.data.body);
+    }
   )
-};
+}
 
   return (
   <div>
-    <Button variant="contained" onClick={getPlanningOrders}> Aktualisieren </Button>
+    <Button  variant="contained" onClick={getPlanningOrders}> Aktualisieren </Button>
     <br/>
     <br/>
     <MUIDataTable 
+
         title={"Planungsaufträge"}
         data={data}
         columns={columns}
