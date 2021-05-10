f7f098fb3dff426068b9e617f88a2bf344aad26b
sauerma <msauer1stud.hs-offenburg.de>
2021-05-10 17:04:48 +0200

    Lambda Funktion GetPlanningOrders - REST in Table

--- src/components/Planung/order_table.js
+++ src/components/Planung/order_table.js
@@ -1,37 +1,108 @@
-import React from "react";
+/*----------------------------------------*/
+  //Author: ESI SoSe21 - Team Production//
+/*----------------------------------------*/
+
+import React, { useState, useEffect} from "react";
 import MUIDataTable from "mui-datatables";
 import Button from '@material-ui/core/Button';
-import axios from 'axios'
+import axios from "axios";
 
 export default function DataTable() {
-  const columns = ["Bestelldatum", "Bestellnr", "Produktionsnr", "Menge", "Status", "Delta E", "Hex-Wert", "Farbe", "Priorität", "Bild"];
-  var data = [
-    ["2021-04-21 11:50:05", "B-20000000-1", "1", "200", "In Planung", "1.324234", "#34923", "Marine", "1", "/home/img/tshirt123123.png"],
-    ["2021-04-21 11:50:05", "B-20000000-1", "2", "21", "In Planung", "1.324234", "#34923", "Rot", "1", "/home/img/tshirt123123.png"],
-    ["2021-04-21 11:50:05", "B-20000000-1", "3", "2", "In Planung", "1.324234", "#34923", "Blau", "2", "/home/img/tshirt123123.png"],
-    ["2021-04-21 11:50:05", "B-20000000-1", "4", "3", "In Produktion", "1.324234", "#34923t", "Grün", "2", "/home/img/tshirt123123.png"],
-    ];
-  const options = {
-  filterType: 'checkbox',
-};
 
+  const columns = ["Bestelldatum", "Bestellnr", "Produktionsnr", "Menge", "Status", "Hex-Wert", "Farbe", "Priorität", "Bild"];
+  const options = { filterType: 'checkbox' };
+  const [data, setData] = useState([])
 
-function getPlanningOrders()
-{
-  axios.get('https://1ygz8xt0rc.execute-api.eu-central-1.amazonaws.com/main/getplanningorders').then(
-    (res) => {
-      console.log(res.data.body);
+  /* 
+  var testdata = [ ["2021-04-21 11:50:05", "B-20000000-1", "1", "200", "In Planung", "1.324234", "#34923", "Marine", "1", "/home/img/tshirt123123.png"],
+      ["2021-04-21 11:50:05", "B-20000000-1", "2", "21", "In Planung", "1.324234", "#34923", "Rot", "1", "/home/img/tshirt123123.png"],
+      ["2021-04-21 11:50:05", "B-20000000-1", "3", "2", "In Planung", "1.324234", "#34923", "Blau", "2", "/home/img/tshirt123123.png"],
+      ["2021-04-21 11:50:05", "B-20000000-1", "4", "3", "In Produktion", "1.324234", "#34923t", "Grün", "2", "/home/img/tshirt123123.png"] ]; */
+
+  useEffect(() => {
+        
+    axios.get('https://1ygz8xt0rc.execute-api.eu-central-1.amazonaws.com/main/getplanningorders')
+        .then(res => {
+        
+        console.log("RESPONSE:", res); //Data from Gateway
+
+        if(IsDataBaseOffline(res)) return; //Check if db is available
+
+        if(res.data.body.length === 0) { //Check if data is available
+          setData(undefined);
+          return;
+        }          
+
+        var sortedOrders = sortOrders(res.data.body); //Sort data from api 
+        if (DataAreEqual(data, sortedOrders)) return; //Check if data has changed       
+        setData(sortedOrders); //Set new data
+  
+        })
+        .catch(err => {
+            console.log(err.message); //Error-Handling
+        })
+  });
+
+  //Check if database is offline (AWS)
+  function IsDataBaseOffline(res){
+
+      if(res.data.errorMessage == null) return false; 
+      if(res.data.errorMessage === 'undefined') return false;
+      if(res.data.errorMessage.endsWith("timed out after 3.00 seconds")){
+          alert("Database is offline (AWS).");
+          return true;
+      }     
+      return false;
     }
-  )
-}
+
+  //Check if old data = new data
+  function DataAreEqual(data, sortedOrders){
+
+    if(data.sort().join(',') === sortedOrders.sort().join(',')){
+      return true;
+      }
+      else return false;
+    }
+
+  //Sort data from api
+  function sortOrders(list){
+
+    var sortedOrders = [];
+
+    for (var key in list){
+
+      var singleSortedList = null;
+      singleSortedList = [];
+      var unsortedOrders = Object.values(list[key]);
+      
+      singleSortedList.push(unsortedOrders[17]); //Bestelldatum
+      singleSortedList.push(unsortedOrders[1]); //Bestellnummer
+      singleSortedList.push(unsortedOrders[18]); //Produktionsnummer
+      singleSortedList.push(unsortedOrders[6]); //Menge
+      singleSortedList.push(StatusNrToBez(unsortedOrders[7])); //Status
+      singleSortedList.push(unsortedOrders[14]); // Hex-Wert
+      singleSortedList.push(unsortedOrders[9]); //Farbe
+      singleSortedList.push(unsortedOrders[15]); //Priorität
+      singleSortedList.push(unsortedOrders[16]); //Bild 
+    
+      sortedOrders.push(singleSortedList)
+  }     
+    return sortedOrders;
+  }
+
+  //Status-Nr to Status-Bez
+  function StatusNrToBez(statusNr){
+    if(statusNr === 0) return "In Planung";
+    if(statusNr === 1) return "In Produktion";
+    if(statusNr === 2) return "Produziert";
+    if(statusNr === 3) return "Eingelagert";
+  }
 
   return (
   <div>
-    <Button  variant="contained" onClick={getPlanningOrders}> Aktualisieren </Button>
     <br/>
     <br/>
     <MUIDataTable 
-
         title={"Planungsaufträge"}
         data={data}
         columns={columns}
