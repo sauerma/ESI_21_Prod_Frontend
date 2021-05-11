6fa8ce828534581f48b7ea13d16e73dbc2e7c66e
dkrieg <dkrieg@stud.hs-offenburg.de>
2021-05-11 18:48:49 +0200

    Added Chatbot

--- src/components/Hilfebereich.js
+++ src/components/Hilfebereich.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import FaqBereich from './Hilfebereich/Faq.js';
-
+import ChatBot from './Hilfebereich/ChatBot.js';
 const Hilfebereich = () => {
     return (
         <div className="maindiv">
             <text>Hilfebereich<br></br> </text>   
            <FaqBereich/>
+           <ChatBot/>
            <br></br>
         </div>
     )
 }
-
 export default Hilfebereich
