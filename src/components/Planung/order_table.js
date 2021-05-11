ffdb2a264a4c3dd4c797eaabfe10aff13d2460cc
dkrieg <dkrieg@stud.hs-offenburg.de>
2021-05-11 17:25:56 +0200

    Create CSV Works now as planned

--- src/components/Planung/order_table.js
+++ src/components/Planung/order_table.js
@@ -6,18 +6,32 @@ import React, { useState, useEffect} from "react";
 import MUIDataTable from "mui-datatables";
 import Button from '@material-ui/core/Button';
 import axios from "axios";
+import { CSVDownload, CSVLink} from "react-csv";
 
 export default function DataTable() {
 
   const columns = ["Bestelldatum", "Bestellnr", "Produktionsnr", "Menge", "Status", "Hex-Wert", "Farbe", "Priorität", "Bild"];
   const options = { filterType: 'checkbox' };
-  const [data, setData] = useState([])
+  const [data, setData] = useState([])  
+  const [csvdata, setCsvData] = useState([])
 
-  
-  var testdata = [ ["2021-04-21 11:50:05", "B-20000000-1", "1", "200", "In Planung", "1.324234", "#34923", "Marine", "1", "/home/img/tshirt123123.png"],
-      ["2021-04-21 11:50:05", "B-20000000-1", "2", "21", "In Planung", "1.324234", "#34923", "Rot", "1", "/home/img/tshirt123123.png"],
-      ["2021-04-21 11:50:05", "B-20000000-1", "3", "2", "In Planung", "1.324234", "#34923", "Blau", "2", "/home/img/tshirt123123.png"],
-      ["2021-04-21 11:50:05", "B-20000000-1", "4", "3", "In Produktion", "1.324234", "#34923t", "Grün", "2", "/home/img/tshirt123123.png"] ]; 
+  //Headerline für CSV export
+  const headers = [
+    { label: "Bestelldatum", key: "Bestelldatum" },
+    { label: "BestellNr", key: "Bestellnr" },
+    { label: "ProduktionsNr", key: "Produktionsnr" },
+    { label: "Menge", key: "Menge" },
+    // { label: "Status", key: "Status" },
+    { label: "Hex-Wert", key: "Hex-Wert" },
+    { label: "Farbe", key: "Farbe" },
+    { label: "Priorität", key: "Priorität" },
+    { label: "Bild", key: "Bild" }
+  ];
+ 
+  // var testdata = [ ["2021-04-21 11:50:05", "B-20000000-1", "1", "200", "In Planung", "1.324234", "#34923", "Marine", "1", "/home/img/tshirt123123.png"],
+  //     ["2021-04-21 11:50:05", "B-20000000-1", "2", "21", "In Planung", "1.324234", "#34923", "Rot", "1", "/home/img/tshirt123123.png"],
+  //     ["2021-04-21 11:50:05", "B-20000000-1", "3", "2", "In Planung", "1.324234", "#34923", "Blau", "2", "/home/img/tshirt123123.png"],
+  //     ["2021-04-21 11:50:05", "B-20000000-1", "4", "3", "In Produktion", "1.324234", "#34923t", "Grün", "2", "/home/img/tshirt123123.png"] ]; 
 
   useEffect(() => {
         
@@ -98,74 +112,72 @@ export default function DataTable() {
     if(statusNr === 3) return "Eingelagert";
   }
 
+  //Get all checked Data in an Obj.Array of Arrays
+  function download_csv_file() {
+    console.log("download_csv_file aufgerufen");
+
+    let targets = Array.from(document.querySelectorAll("td input[type=checkbox]")).filter((elem) => { if(elem.checked) return elem })
+
+    let csv_data = []
+
+    targets.forEach((elem) => {
+        let row = elem.closest(".MuiTableRow-root")
+        let cols = Array.from(row.getElementsByTagName("td"))
+
+        let array = []
+        let obj = {}
+
+        cols.forEach((col) => {
+          let divs = Array.from(col.getElementsByTagName("div"))
+          if(divs.length >= 2){
+            let key = divs[0].innerHTML
+            obj[key] = divs[1].innerHTML
+            array.push(divs[1].innerHTML)
+          }
+        }) 
+        csv_data.push(obj)
+    }) 
+    console.log(csv_data);
+    setCsvData(csv_data);
+    //Function in one Button
+    // return(
+    //   <CSVDownload 
+    //     target="_self"
+    //     headers={headers}
+    //     data={csv_data}
+    //     filename={"Test.csv"}
+    //   />
+    // )
+  }
+
+
   return (
   <div>
     <MUIDataTable 
         title={"Planungsaufträge"}
-        data={data}
+        data={data}      
         columns={columns}
         options={options} />
     <br/>
     <br/>
-    <Button variant="contained" onClick={download_csv_file}>CSV erstellen</Button>
+    <Button variant="contained" onClick={download_csv_file}>In Produktion geben</Button>
+    <br/>
+    <CSVLink data={csvdata} headers={headers} filename={"MachineConfiguration.csv"}>Download CSV Here</CSVLink>
     </div>
   );
 }
 
+    // //REST CALL Create CSV mit array Übergabe  
+    // var data = { title: 'React POST Request Example' };    
+    // axios.post('https://1ygz8xt0rc.execute-api.eu-central-1.amazonaws.com/main/createcsv',data)
+    //     .then(res => {      
+        
+    //     console.log("RESPONSE:", res); //Data from Gateway
 
-function download_csv_file(e) {
+    //   })
+    //     .catch(err => {
+    //         console.error('There was an error!', error); //Error-Handling
+    //     });
 
-  // let targets = document.querySelectorAll("")
-
-  let targets = Array.from(document.querySelectorAll("td input[type=checkbox]")).filter((elem) => { if(elem.checked) return elem })
-
-  let array = []
-  let array2 = []
-
-
-  targets.forEach((elem) => {
-      let row = elem.closest(".MuiTableRow-root")
-      let cols = Array.from(row.getElementsByTagName("td"))
-
-      let obj = {}
-      let array3 = []
-
-      cols.forEach((col) => {
-        let divs = Array.from(col.getElementsByTagName("div"))
-        if(divs.length >= 2){
-          let key = divs[0].innerHTML
-          obj[key] = divs[1].innerHTML
-          array3.push(divs[1].innerHTML)
-        }
-      }) 
-
-      array.push(obj)
-      array2.push(array3)
-  })
-
-  console.log(array)
-  console.log(array2)
-
-  //REST CALL Create CSV mit array Übergabe     
-  axios.post('https://1ygz8xt0rc.execute-api.eu-central-1.amazonaws.com/main/createcsv')
-      .then(res => {
-      
-      console.log("RESPONSE:", res); //Data from Gateway
-
-      // if(IsDataBaseOffline(res)) return; //Check if db is available
-
-      // if(res.data.body.length === 0) { //Check if data is available
-      //   setData(undefined);
-      //   return;
-      // }
-
-      // var sortedOrders = sortOrders(res.data.body); //Sort data from api 
-      // if (DataAreEqual(data, sortedOrders)) return; //Check if data has changed       
-      // setData(sortedOrders); //Set new data
-
-      })
-      .catch(err => {
-          console.log(err.message); //Error-Handling
-      });
-
-}
+  // }
+// }
