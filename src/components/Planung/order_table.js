2c226e1a6bd92d8f36150164a9245d20a9c37f62
sauerma <msauer1stud.hs-offenburg.de>
2021-05-14 16:29:38 +0200

    errorf fix Tabellenauswahl

--- src/components/Planung/order_table.js
+++ src/components/Planung/order_table.js
@@ -12,38 +12,53 @@ import GetAppIcon from '@material-ui/icons/GetApp';
 
 export default function DataTable() {
 
-  const columns = ["Bestelldatum", "Bestellnr", "Produktionsnr", "Menge", "Status", "Hex-Wert", "Priorität", "Bild"];
-  const options = { customToolbarSelect: () => {/* Hide Delete Button */}, filterType: 'checkbox',  
-                    sortOrder: {name: 'Priorität', direction: 'asc'},  download: false, 
+  const columns = [{ name: "O_NR", label: "Bestell-Nr",  options: {filter: true,  sort: true, display: true}}, 
+   {name: "OI_NR", label: "Bestellpos-Nr", options: {filter: true, sort: true, display: true }}, 
+   {name: "PO_CODE", label: "PO_CODE", options: {filter: true,  sort: false,  display: false}}, 
+   {name: "PO_COUNTER", label: "PO_COUNTER", options: {filter: true, sort: false, display: false}},  
+   {name: "O_DATE", label: "Bestelldatum", options: {filter: true, sort: true, display: true}}, 
+   {name: "CUSTOMER_TYPE", label: "Kundentyp", options: {filter: true, sort: true, display: true}}, 
+   {name: "QUANTITY", label: "Menge", options: {filter: true, sort: false, display: true}}, 
+   {name: "PROD_STATUS", label: "Status", options: {filter: true, sort: true, display: true}}, 
+   {name: "MAT_NR", label: "Material-Nr", options: {filter: true, sort: true, display: true}}, 
+   {name: "C", label: "C", options: {filter: true, sort: false, display: false}},
+   {name: "M", label: "M",options: {filter: true,sort: false,display: false}},
+   {name: "Y",label: "Y",options: {filter: true,sort: false, display: false}},
+   {name: "K", label: "K", options: {filter: true,sort: false, display: false}},
+   {name: "HEXCOLOR", label: "Hex-Wert", options: {filter: true,sort: true, display: true}},
+   {name: "PROD_PRIO", label: "Priorität", options: {filter: true,sort: true, display: true}},
+   {name: "IMAGE", label: "Image", options: {filter: true,sort: true, display: true}},
+   {name: "END_DATE",label: "END_DATE",options: {filter: true,sort: false, display: false}},
+   {name: "p_nr", label: "Produktionsnr", options: {filter: true, sort: true, display: true}}];
+
+  const options = { customToolbarSelect: () => {/* Hide Delete Button */}, filterType: 'checkbox', download: false, 
                     onRowSelectionChange : (curRowSelected, allRowsSelected) => {rowSelectEvent(curRowSelected, allRowsSelected); }};
-  const [data, setData] = useState([]);  
   const [csvdata, setCsvData] = useState([]); 
-  const [pkData, setPkData] = useState([]);
-  const [selectedPkData, setSelectedPkData] = useState([]); 
   const [Quantity, setQuantity] = useState("");
   const [QuantityColor, setQuantityColor] = useState("#ffffff");
-  const csvheaders = ["Bestelldatum", "Bestellnr", "Produktionsnr", "Menge", "Hex-Wert", "Bild"];
+  const csvheaders = ["OI_NR", "O_DATE", "p_nr", "QUANTITY", "HEXCOLOR", "IMAGE"];
   
+  const [allData, setAllData] = useState([]); 
+  const [selectedData, setSelectedData] =  useState([]); 
 
 
   useEffect(() => {
 
     axios.get('https://1ygz8xt0rc.execute-api.eu-central-1.amazonaws.com/main/getplanningorders')
         .then(res => {
-        
         console.log("RESPONSE:", res); //Data from Gateway
+        
         if(IsDataBaseOffline(res)) return; //Check if db is available
+
         if(res.data.body.length === 0) { //Check if data is available
-          setData(undefined);
-          setPkData(undefined);
+          setAllData(undefined);
+          setCsvData(undefined);
           return;
         }          
-        var allSortedOrders = sortOrders(res.data.body) //alle geordneten Daten
-        var PKsSortedOrders = allSortedOrders[1] //nur geordnete PKs
-        var sortedTableOrders = allSortedOrders[0]; //nur geordnete Tabellendaten
-        if (DataAreEqual(data, sortedTableOrders)) return; //Check if data has changed       
-        setData(sortedTableOrders); //Set new table data
-        setPkData(PKsSortedOrders); //Set related PKs
+
+        if (DataAreEqual(allData, res.data.body)) return; //Check if data has changed       
+        setAllData(res.data.body); //Set new table data
+
         })
         .catch(err => {
             console.log(err.message); //Error-Handling
@@ -69,42 +84,6 @@ export default function DataTable() {
       else return false;
     }
 
-  //Sort data from api
-  function sortOrders(list){
-    var sortedOrders = [];
-    var sortedPKs = [];
-
-    for (var key in list){
-
-      var singleSortedList = null;
-      singleSortedList = [];
-      var singleSortedPKs = null;
-      singleSortedPKs = [];
-
-      var unsortedOrders = Object.values(list[key]);
-      
-      //PKs
-      singleSortedPKs.push(unsortedOrders[0]) //O_NR
-      singleSortedPKs.push(unsortedOrders[1]) //OI_NR
-      singleSortedPKs.push(unsortedOrders[2]) //PO_CODE
-      singleSortedPKs.push(unsortedOrders[3]) //PO_COUNTER
-     
-      //Table Values
-      singleSortedList.push(unsortedOrders[4]); //Bestelldatum
-      singleSortedList.push(unsortedOrders[1]); //Bestellnummer
-      singleSortedList.push(unsortedOrders[17]); //Produktionsnummer
-      singleSortedList.push(unsortedOrders[6]); //Menge
-      singleSortedList.push(StatusNrToBez(unsortedOrders[7])); //Status
-      singleSortedList.push(unsortedOrders[13]); // Hex-Wert
-      singleSortedList.push(unsortedOrders[14]); //Priorität
-      singleSortedList.push(unsortedOrders[15]); //Bild 
-      
-      sortedOrders.push(singleSortedList);
-      sortedPKs.push(singleSortedPKs);
-  }     
-    return [sortedOrders, sortedPKs];
-  }
-
   //Status-Nr to Status-Bez
   function StatusNrToBez(statusNr){
     if(statusNr === 0) return "In Planung";
@@ -114,78 +93,71 @@ export default function DataTable() {
   }
 
  //Update status in planning to in production
- function update_prod_status(){  
+  function update_prod_status(){  
       
-  if (selectedPkData == null || selectedPkData === undefined || selectedPkData.length === 0) {
-    alert("Bitte Positionen auswählen!");
-    return;
-  }
+    if (selectedData == null || selectedData === undefined || selectedData.length === 0) {
+        alert("Bitte Positionen auswählen!");
+        return;
+      }
 
-  var formatedPks = {}; 
-  var count = 0;  
-  selectedPkData.forEach(element => {
+      var pKs = filterPks(selectedData);
+      console.log("Selected Pks:", pKs);
 
-    var singleObj = {};
-    singleObj["O_NR"] = element[0];
-    singleObj["OI_NR"] = element[1];
-    singleObj["PO_CODE"] = element[2];
-    singleObj["PO_COUNTER"] = element[3];
-    formatedPks[count] = singleObj;
-    count += 1;
+      //TODO Update Production table
 
-  });
+      axios.put("https://1ygz8xt0rc.execute-api.eu-central-1.amazonaws.com/main/updateplanningtoprod", pKs)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err.message); //Error-Handling
+      })
 
- console.log(formatedPks);
-
-  //TODO Update Production table
-
-  axios.put("https://1ygz8xt0rc.execute-api.eu-central-1.amazonaws.com/main/updateplanningtoprod", formatedPks)
-  .then(res => {
-    console.log(res);
-  })
-  .catch(err => {
-    console.log(err.message); //Error-Handling
-  })
-
-  //TODO Update Verkauf & Versand table --> In Lambda-Funktion packen
+      //TODO Update Verkauf & Versand table --> In Lambda-Funktion packen
 
 
  
-  alert("Erfolgreich geupdated.")
-  return;
+    alert("Erfolgreich geupdated.")
+    return;
 }
+
+function filterPks(selectedData){
+  var _pks = {};
+  var counter = 0;
+  selectedData.forEach(element => {
+    var singleVal = {};
+    singleVal["O_NR"] = element["O_NR"];
+    singleVal["OI_NR"] = element["OI_NR"];
+    singleVal["PO_CODE"] = element["PO_CODE"];
+    singleVal["PO_COUNTER"] = element["PO_COUNTER"];
+    _pks[counter] = singleVal;
+    counter += 1;
+  });
+  console.log(_pks);
+  return _pks; 
+}
+
 
   //RowSelectEvent
   function rowSelectEvent(curRowSelected, allRowsSelected){ 
 
-    var selectedData = [];
-    var selectedPKs = [];
+    var _selectedData = [];
 
   if(allRowsSelected.length === 0) {  //Wenn keine Rows ausgewählt sind
       setQuantity(0); //Menge auf 0 setzen
       setQuantityColor("#088A08");
-      setSelectedPkData(undefined);
+      setSelectedData(undefined);
+      setCsvData(undefined);
       return;
     }
-
-    allRowsSelected.forEach(element => {  //Convert all indexes to data array
-  
-      let obj = {}
-
-      columns.forEach((key, i) => {
-        obj[key] = data[element.dataIndex][i]
-      })
-
-      selectedData.push(obj)      
-    }); 
-
-    allRowsSelected.forEach(element => { 
-      selectedPKs.push(pkData[element.index]);
+    
+    allRowsSelected.forEach(element => {
+      _selectedData.push(allData[element.dataIndex])
     });
 
-    updateQuantity(selectedData);  
-    setCsvData(selectedData);
-    setSelectedPkData(selectedPKs);
+    updateQuantity(_selectedData);  
+    setCsvData(_selectedData); 
+    setSelectedData(_selectedData);
 
     return;
   }
@@ -195,7 +167,7 @@ export default function DataTable() {
     var quantity = 0
 
     selecteDataFromTable.forEach(element => {
-      quantity += element["Menge"]
+      quantity += element["QUANTITY"]
     });
     setQuantity(quantity)
 
@@ -209,7 +181,7 @@ export default function DataTable() {
   <div>
     <MUIDataTable 
         title={"Planungsaufträge"}
-        data={data}      
+        data={allData}      
         columns={columns}
         options={options}/>
     <br/>
@@ -217,7 +189,7 @@ export default function DataTable() {
     <Button variant="contained" onClick={update_prod_status} >In Produktion geben</Button>
     <text name="DummySeperator">  </text>
     <Button variant="contained" onClick={update_prod_status} >
-    <CSVLink data={csvdata} headers={csvheaders}  style={{textDecoration: "none", color: "black"}} filename={"MachineConfiguration.csv"}>Download CSV</CSVLink>
+    <CSVLink data={csvdata} headers={csvheaders} style={{textDecoration: "none", color: "black"}} filename={"MachineConfiguration.csv"}>Download CSV</CSVLink>
     <GetAppIcon/>  
     </Button>
     <text name="DummySeperato2">  </text>
