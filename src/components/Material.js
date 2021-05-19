6b174941ed3b62ace22bb86b36627d8465902cfe
sauerma <msauer1stud.hs-offenburg.de>
2021-05-19 21:25:44 +0200

    Contactformular + Materialseiten-Grundriss

--- src/components/Material.js
+++ src/components/Material.js
@@ -1,18 +1,59 @@
 import './Style/Material.css';
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import MatBestellTable from './Material/MatBestellTable.js';
+import ShirtTable from './Material/ShirtTable.js';
+import ColorTable from './Material/ColorTable.js';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
-const Material = () => {
+
+const useStyles = makeStyles((theme) => ({
+ 
+ /*    fixedHeight: {
+      minHeight: 240
+    }, */
+  }));
+
+  const Material = () => {
+
+    const classes = useStyles();
+    const fixedHeightPaper = clsx(classes.fixedHeight);
     return (
-        <div >
-          
+     
+        <div style={{ padding: '0px'}}>
             <Typography paragraph>
-            Material
+            Materialmanagement
             </Typography>
-            <Typography paragraph>
-            Material
-            </Typography>
-        </div>
+        
+        <main >
+   <div /> 
+
+     <Grid container spacing={2}>
+       {/* MatBestellTable */}
+       <Grid item xs={12} md={8} lg={10}>
+        
+           <MatBestellTable />
+      
+       </Grid>
+       {/* ShirtTable */}
+       <Grid item xs={7} md={6} lg={6}>
+         <Paper className={fixedHeightPaper}>
+           <ShirtTable />
+         </Paper>
+       </Grid>
+       {/* ColorTable */}
+       <Grid item xs={7} md={6}>
+         <Paper className={fixedHeightPaper}>
+           <ColorTable />
+         </Paper>
+       </Grid>
+     </Grid>
+
+ </main>
+     </div>
     )
 }
 
