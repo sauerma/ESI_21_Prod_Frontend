a72e33a9ce5df1783ce75275bbd2d52ba4be2ea0
sauerma <msauer1stud.hs-offenburg.de>
2021-05-22 18:27:58 +0200

    Grundriss Materialbestell Table

--- src/components/Material/MatBestellTable.js
+++ src/components/Material/MatBestellTable.js
@@ -1,39 +1,46 @@
 import React, { /*useState, useEffect*/} from "react";
-import MUIDataTable from "mui-datatables";
 import Button from '@material-ui/core/Button';
+import './MatBestellTable.css';
 
 export default function MatBestellTable() {
-        
-    const columns = [{ name: "O_NR", label: "Bestell-Nr",  options: {filter: true,  sort: true, display: true}}, 
-{name: "OI_NR", label: "Bestellpos-Nr", options: {filter: true, sort: true, display: true }}, 
-{name: "PO_CODE", label: "PO_CODE", options: {filter: true,  sort: false,  display: false}}, 
-{name: "PO_COUNTER", label: "PO_COUNTER", options: {filter: true, sort: false, display: false}},  
-{name: "O_DATE", label: "Bestelldatum", options: {filter: true, sort: true, display: true}}, 
-{name: "CUSTOMER_TYPE", label: "Kundentyp", options: {filter: true, sort: true, display: true}}, 
-{name: "QUANTITY", label: "Menge", options: {filter: true, sort: true, display: true}}, 
-{name: "PROD_STATUS", label: "Status", options: {filter: true, sort: true, display: true}}, 
-{name: "MAT_NR", label: "Material-Nr", options: {filter: true, sort: true, display: false}}, 
-{name: "C", label: "C", options: {filter: true, sort: false, display: false}},
-{name: "M", label: "M",options: {filter: true,sort: false,display: false}},
-{name: "Y",label: "Y",options: {filter: true,sort: false, display: false}},
-{name: "K", label: "K", options: {filter: true,sort: false, display: false}},
-{name: "HEXCOLOR", label: "Hex-Wert", options: {filter: true,sort: true, display: true}},
-{name: "PROD_PRIO", label: "Priorität", options: {filter: true,sort: true, display: false}},
-{name: "IMAGE", label: "Image", options: {filter: true,sort: true, display: true}},
-{name: "END_DATE", label: "Enddatum",options: {filter: true,sort: false, display: true}},
-{name: "p_nr", label: "Produktionsnr", options: {filter: true, sort: true, display: true}}];
-
-const options = {rowsPerPage: 8, customToolbarSelect: () => {/* Hide Delete Button */}, filterType: 'checkbox' };
-var data = undefined;
-
+  
 
   return (
-    <div style={{ padding: '0px'}}>
-    <MUIDataTable
-title={"Material - Bestellung"}
-data={data}
-columns={columns}
-options={options} />
+    <div >
+  <table className="matTable">
+
+        <tr  className="header">
+            <th colspan="1">T-Shirt</th>
+            <th colspan="2">T-Shirt divers</th>
+            <th colspan="3">C-Farbe</th>
+            <th colspan="4">M-Farbe</th>
+            <th colspan="5">Y-Farbe</th>
+            <th colspan="6">K-Farbe</th>
+        </tr>
+        <tr>
+            <td colspan="1" >#FFFFFF</td>
+            <td colspan="2">
+             <select name="coloordivers" id="colordiv">
+              <option value="1">#55343</option>
+              <option value="2">#zr4tz</option>
+              <option value="3">#23343</option>
+              <option value="4">#2344</option>
+             </select>
+            </td>
+            <td colspan="3" className="color1"> </td>
+            <td colspan="4" className="color2"> </td>
+            <td colspan="5" className="color3"> </td>
+            <td colspan="6" className="color4"> </td>
+        </tr>
+        <tr >
+        <td colspan="1"><input type="text" id="tshirt" name="tshirt"/>Stk</td>
+            <td colspan="2"><input type="text" id="tshirtdiv" name="tshirtdiv"/>Stk</td>
+            <td colspan="3"><input type="text" id="c" name="c"/>Liter</td>
+            <td colspan="4"><input type="text" id="m" name="m"/>Liter</td>
+            <td colspan="5"><input type="text" id="y" name="y"/>Liter</td>
+            <td colspan="6"><input type="text" id="k" name="k"/>Liter</td>
+        </tr>
+</table>
 <br></br>
 <Button variant="contained" stitle="Mit Klick auf diesen Button
     werden die entsprechenden T-Shirts oder Farben bestellt.">Bestellen</Button>
